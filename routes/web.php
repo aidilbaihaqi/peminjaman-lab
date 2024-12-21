4f2cca571a05805f8146992b5bfe8ba12dcfd1af
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-12-22 05:17:50 +0700

    group routing

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,33 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LaboratoriumController;
+use App\Http\Controllers\PeminjamanController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Laboratorium;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Dashboard
 Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-Route::get('/admin/data-laboratorium', [LaboratoriumController::class, 'index'])->name('laboratorium.index');
-Route::get('/admin/tambah-laboratorium', [LaboratoriumController::class, 'create'])->name('laboratorium.create');
-Route::post('/admin/tambah-laboratorium', [LaboratoriumController::class, 'store'])->name('laboratorium.store');
-Route::get('/admin/edit-laboratorium/{id}', [LaboratoriumController::class, 'edit'])->name('laboratorium.edit');
-Route::post('/admin/edit-laboratorium/{id}', [LaboratoriumController::class, 'update'])->name('laboratorium.update');
-Route::get('/admin/hapus-laboratorium/{id}', [LaboratoriumController::class, 'destroy'])->name('laboratorium.destroy');
+
+// Laboratorium
+Route::controller(LaboratoriumController::class)->group(function() {
+    Route::get('/admin/data-laboratorium', 'index')->name('laboratorium.index');
+    Route::get('/admin/tambah-laboratorium', 'create')->name('laboratorium.create');
+    Route::post('/admin/tambah-laboratorium', 'store')->name('laboratorium.store');
+    Route::get('/admin/edit-laboratorium/{id}', 'edit')->name('laboratorium.edit');
+    Route::post('/admin/edit-laboratorium/{id}', 'update')->name('laboratorium.update');
+    Route::get('/admin/hapus-laboratorium/{id}', 'destroy')->name('laboratorium.destroy');
+}); 
+
+// Peminjaman
+Route::controller(PeminjamanController::class)->group(function() {
+    Route::get('/admin/data-peminjaman', 'index')->name('peminjaman.index');
+    Route::get('/admin/tambah-peminjaman', 'create')->name('peminjaman.create');
+    Route::post('/admin/tambah-peminjaman', 'store')->name('peminjaman.store');
+    Route::get('/admin/edit-peminjaman/{id}', 'edit')->name('peminjaman.edit');
+    Route::post('/admin/edit-peminjaman/{id}', 'update')->name('peminjaman.update');
+    Route::get('/admin/hapus-peminjaman/{id}', 'destroy')->name('peminjaman.destroy');
+});
 
 
 Route::get('/dashboard', function () {
